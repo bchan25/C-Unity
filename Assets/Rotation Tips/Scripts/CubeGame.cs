7bc33c26cbd00e1a3744ba4f39ce59f6b1d2aaa2
Ben Chan <ben@chanhq.com>
2020-04-01 19:39:49 +0100

    Simple if statement game

---
+++ Assets/Rotation Tips/Scripts/CubeGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGame : MonoBehaviour
+{
+    public GameObject cube;
+    public int score = 0;
+
+    Renderer cubeRenderer;
+
+    private bool hasCalled = false;
+
+    void Awake()
+    {
+        cubeRenderer = cube.GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        cubeRenderer.material.SetColor("_Color", Color.red);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !hasCalled)
+        {
+            score += 10;
+        }
+
+        if(score == 50 && !hasCalled)
+        {
+            hasCalled = true;
+            cubeRenderer.material.SetColor("_Color", Color.green);
+            Invoke("ResetGame", 1.5f);
+        }
+    }
+
+    void ResetGame()
+    {
+        score = 0;
+        hasCalled = false;
+        cubeRenderer.material.SetColor("_Color", Color.red);
+    }
+}
