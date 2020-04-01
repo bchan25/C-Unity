6c4ebd3a518806f07b835a866eaa574ab1f6461e
Ben Chan <ben@chanhq.com>
2020-04-01 19:18:34 +0100

    Made debug log called once

--- Assets/If Statement/Script/ChallengeOne.cs
+++ Assets/If Statement/Script/ChallengeOne.cs
@@ -7,6 +7,8 @@ public class ChallengeOne : MonoBehaviour
     [SerializeField]
     private int score = 0;
 
+    private bool calledMessage = false;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && score != 50)
@@ -14,8 +16,9 @@ public class ChallengeOne : MonoBehaviour
             score += 10;
         }
 
-        if(score == 50)
+        if(score == 50 && !calledMessage)
         {
+            calledMessage = true;
             Debug.Log("You win");
             Debug.Log("Game Over");
             StartCoroutine(ResetGameRoutine());
@@ -27,5 +30,6 @@ public class ChallengeOne : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         score = 0;
         Debug.Log("Game Reset");
+        calledMessage = false;
     }
 }
