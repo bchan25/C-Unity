b07e55c36562dbd03a3c7332b4ff9bee5f33ffbe
Ben Chan <ben@chanhq.com>
2020-04-01 15:00:51 +0100

    Added quick tips folder and Change position example

---
+++ Assets/Quick Tips/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 startPos;
+    
+    void Start()
+    {
+        // Change the position when game start
+        transform.position = startPos;
+    }
+
+}
