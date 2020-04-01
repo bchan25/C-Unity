7ded93001c16d327ba188a8c2ad27f0b9651932f
Ben Chan <ben@chanhq.com>
2020-04-01 16:26:50 +0100

    LookAt example implemented

---
+++ Assets/Rotation Tips/Scripts/Aim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    
+    void Update()
+    {
+        // direction = destination - source
+        Vector3 direction = target.position - transform.position;
+
+        // Debug raw
+        Debug.DrawRay(transform.position, direction, Color.blue);
+
+        // Look this direction
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
