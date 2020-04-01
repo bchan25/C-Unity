89f0465ce4cdd839350b37f8cc7c52eeb4783324
Ben Chan <ben@chanhq.com>
2020-04-01 15:06:52 +0100

    User input example implemented

---
+++ Assets/Quick Tips/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    
+    void Update()
+    {
+        // User Input
+        // If user press space button
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Spacebar pressed");
+        }
+
+        // if e key held down
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Holding");
+        }
+
+        // if f key lifted up
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            Debug.Log("F key lifted up");
+        }
+    }
+}
