dfafe1a1e2f49985428381aeed7326e7ed709f18
Ben Chan <ben@chanhq.com>
2020-04-01 16:35:02 +0100

    Rotation Slerp example implemented

---
+++ Assets/Rotation Tips/Scripts/AimTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTransition : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void Update()
+    {
+        // direction = destination - source
+        Vector3 direction = target.position - transform.position;
+
+        // Target rotation
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        // Interpolate value
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+
+    }
+}
