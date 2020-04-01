8e9f9068997afa6ec20739481b69c5ec183657f6
Ben Chan <ben@chanhq.com>
2020-04-01 15:20:07 +0100

    Simple movement with input example implemented

---
+++ Assets/Quick Tips/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector3 move = new Vector3(horizontalInput, transform.position.y, transform.position.z);
+
+        // Move player with the translate
+        transform.Translate(move * _speed * Time.deltaTime);
+    }
+}
