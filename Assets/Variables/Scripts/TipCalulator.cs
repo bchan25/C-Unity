81acb0a950952373ebe31d28b03451869a5899d0
Ben Chan <ben@chanhq.com>
2020-04-01 17:09:38 +0100

    Tip calculator implemented

---
+++ Assets/Variables/Scripts/TipCalulator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalulator : MonoBehaviour
+{
+    public float totalBill;
+
+    public float tipPercentage = 20.0f;
+
+    private float tipAmount;
+    private float totalBillAmount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tipAmount = totalBill * (tipPercentage / 100);
+        totalBillAmount = totalBill + tipAmount;
+
+        Debug.Log("Tip Amount: " + tipAmount.ToString("c2"));
+        Debug.Log("Total Bill: " + totalBillAmount.ToString("c2"));
+    }
+
+}
