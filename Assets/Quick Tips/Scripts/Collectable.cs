7e78c960da09ec28f88a2c927fd1717ed9978bb0
Ben Chan <ben@chanhq.com>
2020-04-01 15:30:55 +0100

    Collectable example implemented

---
+++ Assets/Quick Tips/Scripts/Collectable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Power Up");
+            Destroy(this.gameObject);
+        }
+    }
+}
