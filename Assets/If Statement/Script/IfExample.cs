fc9700d024b330c35afe05e5eac09af58d7608ca
Ben Chan <ben@chanhq.com>
2020-04-01 19:01:56 +0100

    If example implemented

---
+++ Assets/If Statement/Script/IfExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfExample : MonoBehaviour
+{
+    public int myAge = 32;
+
+    void Start()
+    {
+        // IF CONDITION
+        // THEN RUN CODE
+
+        // If myAge is greater than or equal to 17
+        // then print out you can drive
+        if(myAge >= 17)
+        {
+            Debug.Log("You can drive");
+        }
+        else
+        {
+            Debug.Log("You can't drive");
+        }
+
+    }
+
+    
+}
