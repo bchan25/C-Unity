09550473481a86f5e3d73d0ba2af0a4bbcbbfee2
Ben Chan <ben@chanhq.com>
2020-04-01 15:41:37 +0100

    Pause example implemented

---
+++ Assets/Quick Tips/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    
+    void Update()
+    {
+        // Pause
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0;
+            Debug.Log("Game Paused");
+        }
+
+        // Unpaused
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            Debug.Log("Game Unpaused");
+        }
+
+        // Slow Mo
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Time.timeScale = 0.5f;
+            Debug.Log("Slow Mo");
+            StartCoroutine(SpeedUpRoutine());
+        }
+    }
+
+    IEnumerator SpeedUpRoutine()
+    {
+        Debug.Log("Speed Up Routine Stated");
+        yield return new WaitForSeconds(2f);
+        Time.timeScale = 1f;
+        Debug.Log("Normal speed");
+    }
+}
