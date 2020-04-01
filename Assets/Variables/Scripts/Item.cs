b4dd3ea113779fde401151e684b45f0ff6f12ff8
Ben Chan <ben@chanhq.com>
2020-04-01 16:57:20 +0100

    Variable Challenge

---
+++ Assets/Variables/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public string itemName;
+    public string itemdescription;
+    public Sprite itemImageIcon;
+    public float itemAttackStrength;
+
+
+    void Start()
+    {
+        
+    }
+
+}
