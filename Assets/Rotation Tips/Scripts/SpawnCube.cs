913827eb10bdc4ca1d8027acac66683c4efc1073
Ben Chan <ben@chanhq.com>
2020-04-01 16:19:52 +0100

    Rotation example implemented

---
+++ Assets/Rotation Tips/Scripts/SpawnCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cubePrefab;
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // ZERO Rotation 0, 0, 0
+            // Instantiate(cubePrefab, Vector3.zero, Quaternion.identity);
+
+            // 35 degrees on Y
+            Instantiate(cubePrefab, Vector3.zero, Quaternion.Euler(0, 35, 0));
+        }
+    }
+}
