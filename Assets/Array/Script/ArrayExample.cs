41db24a545f1cd0901367a52c9c6a3e89e2a7bd3
Ben Chan <ben@chanhq.com>
2020-04-02 10:28:45 +0100

    Array example implemented

---
+++ Assets/Array/Script/ArrayExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayExample : MonoBehaviour
+{
+
+    // Array holds multiple values
+    public string[] names;
+
+    public string[] items = new string[5];
+
+    public int[] ages = new int[] { 5, 4, 3, 2, 1 };
+
+    private void Start()
+    {
+        // Access a array element
+        Debug.Log(ages[3]); // prints 2
+    }
+}
