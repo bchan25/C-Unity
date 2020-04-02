4284ec8cb8c4633ec14041a1ea77f6c23ab2edc6
Ben Chan <ben@chanhq.com>
2020-04-02 09:19:51 +0100

    Switch example implemented

---
+++ Assets/Switch/Scripts/SwitchExample.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchExample : MonoBehaviour
+{
+    public int selectedLevel;
+
+    private int _easy = 0;
+    private int _medium = 1;
+    private int _hard = 2;
+
+    private void Start()
+    {
+        selectedLevel = Random.Range(0, 3);
+    }
+
+    private void Update()
+    {
+        switch (selectedLevel)
+        {
+            case 0:
+                Debug.Log("Easy level selected");
+                break;
+            case 1:
+                Debug.Log("Medium level selected");
+                break;
+            case 2:
+                Debug.Log("Hard level selected");
+                break;
+            default:
+                Debug.Log("Not valid level");
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            selectedLevel = Random.Range(0, 3);
+        }
+    }
+}
