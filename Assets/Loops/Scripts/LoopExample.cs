c4d0a946f3d119257c713d302659213c33ccefa1
Ben Chan <ben@chanhq.com>
2020-04-02 09:53:37 +0100

    For loop example implemented

---
+++ Assets/Loops/Scripts/LoopExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopExample : MonoBehaviour
+{
+
+    public string myName = "Ben";
+
+    void Start()
+    {
+        // For Loop
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(myName);
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
