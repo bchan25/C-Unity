ed6927fdcd12a4cd6852127df6b7723de18806be
Ben Chan <ben@chanhq.com>
2020-04-02 11:01:04 +0100

    Function examples

---
+++ Assets/Functions Methods/Scripts/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public GameObject cube;
+    public Color newColor;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeObjectColor(cube, newColor);
+        }
+    }
+
+    private void ChangeObjectColor(GameObject cube, Color color)
+    {
+        cube.GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+}
---
+++ Assets/Functions Methods/Scripts/FunctionExample.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionExample : MonoBehaviour
+{
+    
+    void Start()
+    {
+        // Called Method
+        MyMethod();
+        Add(4, 5);
+        Debug.Log(Subtract(4, 2));
+    }
+
+    
+    void Update()
+    {
+       
+    }
+
+    // Method
+    private void MyMethod()
+    {
+        Debug.Log("MyMethod called");
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack Code");
+    }
+
+    private void PowerUp()
+    {
+        Debug.Log("Powerup Code");
+    }
+
+    // Parameters
+    private void Add(int a, int b)
+    {
+        Debug.Log(a + b);
+    }
+
+    // Return Type
+    private int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
